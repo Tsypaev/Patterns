ee0bc2e28b87464cbdb9dc01ad44859907a7ba13
Tsypaev Vladimir <tsypaevova@yandex.ru>
2018-12-07 13:01:47 +0500

    Add multi where condition

--- src/main/java/ru/tsypaev/patterns/builder/SQL.java
+++ src/main/java/ru/tsypaev/patterns/builder/SQL.java
@@ -1,5 +1,6 @@
 package ru.tsypaev.patterns.builder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class SQL {
@@ -8,9 +9,9 @@ class SQL {
 
         private String[] rows;
         private String[] table;
-        private String sign;
-        private String row;
-        private String value;
+
+        private ArrayList whereCondition = new ArrayList();
+
 
         public QueryBuilder select(String... select) {
             this.rows = select;
@@ -22,27 +23,42 @@ class SQL {
             return this;
         }
 
-        public QueryBuilder where(String where, String parameter, String sign) {
-            this.row = where;
-            this.value = parameter;
-            this.sign = sign;
+        public QueryBuilder where(String row, String sign, String value) {
+            whereCondition.add(row);
+            whereCondition.add(sign);
+            whereCondition.add(value);
             return this;
         }
 
         @Override
         public String toString() {
-            if (row != null) {
-                return  "SELECT " + getArrayString(rows) +
+
+            if (whereCondition.size() != 0) {
+                return "SELECT " + getArrayString(rows) +
                         " FROM " + getArrayString(table) +
-                        " WHERE " + row + sign + value;
+                        " WHERE " + getWhereConditionString();
             } else {
                 return "SELECT " + getArrayString(rows) +
                         " FROM " + getArrayString(table);
             }
         }
 
+        private String getWhereConditionString() {
+            String[] array = (String[]) whereCondition.toArray(new String[0]);
+            StringBuilder whereCondition = new StringBuilder();
+            for (int i = 1; i < array.length + 1; i++) {
+                if (i % 3 == 0) {
+                    whereCondition.append(array[i - 1] + " AND ");
+                } else {
+                    whereCondition.append(array[i - 1]);
+                }
+            }
+            whereCondition.delete(whereCondition.lastIndexOf(" AND"), whereCondition.length());
+            return whereCondition.toString();
+        }
+
         private String getArrayString(String[] array) {
-            return Arrays.toString(array).replaceAll("[\\[\\]]","");
+            return Arrays.toString(array).replaceAll("[\\[\\]]", "");
         }
     }
 }
--- src/test/java/ru/tsypaev/patterns/builder/SqlBuilderTest.java
+++ src/test/java/ru/tsypaev/patterns/builder/SqlBuilderTest.java
@@ -9,17 +9,19 @@ class SqlBuilderTest {
     @Test
     void makeCorrectQueryWithoutWhereCondition() {
         SQL.QueryBuilder sqlBuild = new SQL.QueryBuilder()
-                .select("id","customer")
+                .select("id", "customer")
                 .from("payment");
-        assertEquals(sqlBuild.toString(),"SELECT id, customer FROM payment");
+        assertEquals(sqlBuild.toString(), "SELECT id, customer FROM payment");
     }
 
     @Test
     void makeCorrectQueryWithWhereCondition() {
         SQL.QueryBuilder sqlBuild = new SQL.QueryBuilder()
-                .select("id","customer")
+                .select("id", "customer")
                 .from("payment")
-                .where("age","18", ">");
-        assertEquals(sqlBuild.toString(),"SELECT id, customer FROM payment WHERE age>18");
+                .where("age", ">", "18")
+                .where("weight", "<=", "80")
+                .where("height", "=", "176");
+        assertEquals(sqlBuild.toString(), "SELECT id, customer FROM payment WHERE age>18 AND weight<=80 AND height=176");
     }
 }
